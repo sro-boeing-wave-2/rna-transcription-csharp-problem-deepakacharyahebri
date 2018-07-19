4a1fb25a961f0abe39b639eeec20fcd0a9a2bd6b
Deepak Acharya <aacharyadeepak100@gmail.com>
2018-07-19 11:09:27 +0530

    RNA transcription solution has been uploaded

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -6,7 +6,27 @@ namespace RNATranscriptionModule
     {
         public static string ToRna(string nucleotide)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            //throw new NotImplementedException("You need to implement this function.");
+            string OutputNucleotide="";
+            for (int i = 0; i < nucleotide.Length; i++)
+            {
+                switch (nucleotide[i])
+                {
+                    case 'G':
+                        OutputNucleotide += 'C';
+                        break;
+                    case 'C':
+                        OutputNucleotide += 'G';
+                        break;
+                    case 'T':
+                        OutputNucleotide += 'A';
+                        break;
+                    case 'A':
+                        OutputNucleotide += 'U';
+                        break;
+                }
+            }
+            return OutputNucleotide;
         }
     }
 }
